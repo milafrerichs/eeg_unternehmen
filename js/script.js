45c8540bb22e79fbdebfc190b383996fdf926b71
Mila Frerichs <mila.frerichs@gmail.com>
2013-02-26 21:15:01 +0100

    added top 20 companies and timeline

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 (function() {
-  var branchen, bundeslaender, bundeslaender_abkuerzungen, bundesland_branchen, per_bundesland;
+  var branchen, branchen_colors, bundeslaender, bundeslaender_abkuerzungen, bundeslaender_colors, bundesland_branchen, gesamt, per_bundesland, top_20_unternehmen;
 
   bundeslaender = [];
+
+  bundeslaender_colors = ["#D47B8C", "#6EB042", "#9067D9", "#729FC6", "#CA9839", "#D44B33", "#50A58B", "#87397E", "#516830", "#515572", "#D54ED1", "#D83F7D", "#873840", "#C786C8", "#9E6133", "#5B70C3"];
+
+  branchen_colors = ["#559437", "#DE7DCC", "#E08131", "#69D0D5", "#D25D69", "#D8E43D", "#BAAA96", "#6E6E9B", "#498F6C", "#8A7F2B", "#62DEAC", "#D8AED0", "#63DC71", "#A7707E", "#D4CE89", "#5F8083", "#8DDE3F", "#6EA7D6", "#E4B03D", "#8E6950", "#A38DD9", "#AED9B1", "#E09F7C", "#C9D5D7", "#C76190", "#A8DA7A", "#6F834C", "#A56A2A", "#CE6047", "#C2C249"];
 
   bundeslaender_abkuerzungen = function(bundesland) {
     var abkuerzung;
@@ -107,7 +111,7 @@
           return d.Count;
         })
       ]).range([0, 20]);
-      color = d3.scale.category20();
+      color = d3.scale.ordinal().range(branchen_colors);
       g = arc_group.selectAll("path").data(pie(data)).enter();
       label = label_group.selectAll("path").data(pie(data)).enter();
       center = center_group.selectAll("text").data(pie(data)).enter();
@@ -122,7 +126,7 @@
         d3.select($('.center_group text:eq(' + i + ')')[0]).style('opacity', '0');
         return d3.select(this).transition().duration(750).attr("d", arc);
       });
-      center.append("text").attr("transform", "translate(0,0)").style("opacity", "0").style("text-anchor", "middle").style("font-size", "40px").text(function(d) {
+      center.append("text").attr("transform", "translate(0,-30)").style("opacity", "0").style("text-anchor", "middle").style("font-size", "40px").text(function(d) {
         return d.data.Count;
       });
       return label.append("text").attr("transform", "translate(0,0)").style("font-size", "12px").style("text-anchor", "middle").style("opacity", "0").text(function(d) {
@@ -140,7 +144,7 @@
     svg = d3.select("#bundeslaender_container").append("svg").attr("id", "per_bundesland").attr("width", pb_width + pb_padding * 2 + 50).attr("height", pb_height + pb_padding * 2 + 90);
     balken = svg.append("svg:g").attr("class", "bundesland");
     labels = svg.append("svg:g").attr("class", "labels");
-    colors = d3.scale.category20c();
+    colors = d3.scale.ordinal().domain(bundeslaender).range(bundeslaender_colors);
     return d3.csv("/eeg_unternehmen/per_bundesland.csv", function(error, data) {
       var g, scale, xAxis, yAxis, y_scale;
       scale = d3.scale.linear().domain([
@@ -160,8 +164,11 @@
       }).attr("width", pb_width / data.length - bar_padding).attr("height", function(d) {
         return scale(d.Count);
       }).attr("fill", function(d, i) {
-        return "rgb(0, " + (155 - i * 10) + ", " + (255 - i * 10) + ")";
+        return colors(d.Bundesland);
       }).on("mouseover", function(d, i) {}).on("mouseout", function(d, i) {});
+      g.append("text").text(function(d) {
+        return d.Count;
+      });
       labels.selectAll("text").data(data).enter().append("text").text(function(d) {
         return bundeslaender_abkuerzungen(d.Bundesland);
       }).attr("x", function(d, i) {
@@ -171,6 +178,82 @@
       xAxis = d3.svg.axis().scale(d3.scale.linear().domain([1, 16]).range([1, pb_width + pb_padding])).orient("bottom").tickValues(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
       svg.append("g").attr("class", "axis").attr("transform", "translate(0," + pb_height + ")").call(xAxis);
       return svg.append("g").attr("class", "y axis").attr("transform", "translate(" + (pb_width + pb_padding) + "," + pb_padding + ")").call(yAxis);
+    });
+  };
+
+  gesamt = function() {
+    var g1, g2, height, line, line2, margin, parseDate, svg, width, x, xAxis, y, y2, y2Axis, yAxis;
+    margin = {
+      top: 20,
+      right: 80,
+      bottom: 30,
+      left: 80
+    };
+    width = 600 - margin.left - margin.right;
+    height = 300 - margin.top - margin.bottom;
+    x = d3.time.scale().range([0, width]);
+    y = d3.scale.linear().range([height, 0]);
+    y2 = d3.scale.linear().range([height, 0]);
+    xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(d3.time.years, 1);
+    yAxis = d3.svg.axis().scale(y).orient("left");
+    y2Axis = d3.svg.axis().scale(y2).orient("right");
+    line = d3.svg.line().x(function(d) {
+      return x(d.Jahr);
+    }).y(function(d) {
+      return y(d.Anzahl);
+    });
+    line2 = d3.svg.line().x(function(d) {
+      return x(d.Jahr);
+    }).y(function(d) {
+      return y2(d.Verbrauch);
+    });
+    parseDate = d3.time.format("%Y").parse;
+    svg = d3.select("#gesamt_line").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    g1 = svg.append("svg:g").attr("class", "anzahl").attr("transform", "translate(0,0)");
+    g2 = svg.append("svg:g").attr("class", "verbrauch").attr("transform", "translate(0,0)");
+    return d3.csv("/eeg_unternehmen/gesamt.csv", function(error, data) {
+      data.map(function(d) {
+        d.Jahr = parseDate(d.Jahr);
+        d.Anzahl = parseInt(d.Anzahl);
+        return d.Verbrauch = parseInt(d.Verbrauch);
+      });
+      x.domain(d3.extent(data, function(d) {
+        return d.Jahr;
+      }));
+      y.domain([
+        0, d3.max(data, function(d) {
+          return d.Anzahl;
+        })
+      ]);
+      y2.domain([
+        0, d3.max(data, function(d) {
+          return d.Verbrauch;
+        })
+      ]);
+      svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+      svg.append("g").attr("class", "y axis").attr("transform", "translate(-15,0)").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Anzahl Unternehmen").attr("class","anzahl");
+      svg.append("g").attr("class", "y axis ").attr("transform", "translate(" + (width + 15) + ",0)").call(y2Axis).append("text").attr("transform", "rotate(-90)").attr("y", -16).attr("dy", ".71em").style("text-anchor", "end").text("Verbrauch in GwH").attr("class","verbrauch");
+      g1.append("path").datum(data).attr("class", "line").attr("d", line);
+      return g2.append("path").datum(data).attr("class", "line 2").attr("d", line2);
+    });
+  };
+
+  top_20_unternehmen = function() {
+    var table, tbody, thead;
+    table = d3.select("#top_20").append("table").attr("class","table-striped table-bordered");
+    thead = table.append("thead").append("tr");
+    thead.append("td").text("Firma");
+    thead.append("td").text("# Niederlassungen");
+    tbody = table.append("tbody");
+    return d3.csv("/eeg_unternehmen/niederlassungen_top_20.csv", function(error, data) {
+      var tr;
+      tr = tbody.selectAll("tr").data(data).enter().append("tr");
+      tr.append("td").text(function(d) {
+        return d.Firma;
+      });
+      return tr.append("td").text(function(d) {
+        return d.Count;
+      });
     });
   };
 
@@ -191,7 +274,9 @@
       })();
     });
     branchen();
-    return per_bundesland();
+    per_bundesland();
+    gesamt();
+    return top_20_unternehmen();
   });
 
 }).call(this);
